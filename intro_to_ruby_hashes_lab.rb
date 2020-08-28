98067778723b0b4bc9cbc0767352bf851b4e8c16
Barbara Kuofie <barbara.kuofie@gmail.com>
2020-08-28 23:50:53 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -36,7 +36,7 @@ def monopoly_with_third_tier
       :names => {
         :reading_railroad => {
          },
-        :pennslyvania_railroad => {
+        :pennsylvania_railroad => {
          },
         :b_and_o_railroad => {
          },
@@ -65,7 +65,7 @@ def monopoly_with_fourth_tier
         :reading_railroad => {
           :mortgage_value => 100
          },
-        :pennslyvania_railroad => {
+        :pennsylvania_railroad => {
           :mortgage_value => 200
          },
         :b_and_o_railroad => {
